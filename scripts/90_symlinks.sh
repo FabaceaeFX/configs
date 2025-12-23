1d650652048006c4fb2e2508c47b34cba1568a78
phil <fabaceae@protonmail.com>
2025-12-23 12:32:29 +0100

    added symlinks to scripts folder

---
+++ scripts/90_symlinks.sh
@@ -0,0 +1,19 @@
+
+#!/usr/bin/env bash
+set -e
+
+echo "▶ Creating symlinks for dotfiles"
+
+# Ensure config directory exists
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.config/i3"
+
+# i3
+ln -sf "$HOME/dotfiles/i3/*" "$HOME/.config/i3/config"
+# tmux
+ln -sf "$HOME/dotfiles/tmux/tmux.conf" "$HOME/.tmux.conf"
+# neovim
+ln -sf "$HOME/dotfiles/nvim/*" "$HOME/.config/nvim"
+# zsh
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/dotfiles/zsh/.zprofile" "$HOME/.zprofile"
